3aae2e4fcb5c7b565e58b31132cb565301cf9d20
Kukkis66 <matti.kukkasjarvi@gmail.com>
2023-10-05 13:44:47 +0300

    Tehtävä 2.11

--- Osa2/Puhelinluettelo/src/App.jsx
+++ Osa2/Puhelinluettelo/src/App.jsx
@@ -1,21 +1,26 @@
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Persons } from './components/Persons'
-import { useState } from 'react'
 import { PersonForm } from './components/PersonForm'
 import { Finder } from './components/Finder'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchItem, setFinder] = useState('')
   
-  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes')
   
   
   const addName = (event) => {
@@ -45,12 +50,7 @@ const App = () => {
   const searchTerm = event.target.value
     setFinder(searchTerm)
   
-   
-
-  
-  
-
- }
+  }
  
  
   const handleNewPerson = (event) => {
