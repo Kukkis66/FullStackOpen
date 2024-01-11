ed691b7bee309871c771b6f01aa5fba2980010d5
Kukkis66 <matti.kukkasjarvi@gmail.com>
2024-01-11 21:52:05 +0200

    pages

--- Osa2/Kurssitiedot/vite.config.js
+++ Osa2/Kurssitiedot/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/FullStackOpen",
+  base: "/FullStackOpen/Osa2/Kurssitiedot",
   plugins: [react()],
 })
